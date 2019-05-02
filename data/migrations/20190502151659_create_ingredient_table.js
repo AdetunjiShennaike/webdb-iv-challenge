99bcfc3220e690f6d90ac7be63f2b3577b1a98ce
Adetunji <ashennaike@gmail.com>
2019-05-02 18:11:26 -0400

    added the tables for migrations

--- data/migrations/20190502151659_create_ingredient_table.js
+++ data/migrations/20190502151659_create_ingredient_table.js
@@ -1,8 +1,21 @@
 
 exports.up = function(knex, Promise) {
+  return knex.schema.createTable('ingredients', tbl => {
+    //primary key
+    tbl.increments();
+
+    //table data
+    //name
+    tbl
+    .string('name', '128')
+    .notNullable()
+    .unique()
+
+    
+  })
   
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('ingredients');
 };
--- data/migrations/20190502152531_create_recing_table.js
+++ data/migrations/20190502152531_create_recing_table.js
@@ -1,8 +1,37 @@
 
 exports.up = function(knex, Promise) {
+  return knex.schema.createTable('recIng', tbl => {
+    //primary key
+    tbl.increments();
+    
+    //foreign keys
+    tbl
+    .integer('recipe_id')
+    .unsigned()
+    .references('id')
+    .inTable('recipes')
+    .notNullable()
+    .onDelete('RESTRICT')
+    .onUpdate('CASCADE')
+    
+    tbl
+    .integer('ingredient_id')
+    .unsigned()
+    .references('id')
+    .inTable('ingredients')
+    .notNullable()
+    .onDelete('RESTRICT')
+    .onUpdate('CASCADE')
+    
+    //table data
+    //amount required
+    tbl
+    .float('amount')
+    .notNullable()
+  })
   
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('recIng');
 };
