01d1f89748e82ef8f16f1f78e2680102675fdd72
Adetunji <ashennaike@gmail.com>
2019-05-02 15:15:47 -0400

    basic setup for routes, server running

---
+++ routes/dishRoute.js
@@ -0,0 +1,26 @@
+//import express and set route
+const express = require('express');
+const route = express.Router();
+
+//import db
+
+
+//set error msgs
+const sendError = (msg, res) => {
+  res.status(500).json({ error: `${msg}`});
+};
+
+const missingError = res => {
+  res.status(404).json({ error: 'This action does not exist'});
+};
+
+const newError = (sts, msg, res) => {
+  res.status(sts).json({ error: `${msg}` })
+}
+
+//set middleware
+
+//CRUD requests
+
+//export
+module.exports = route;
