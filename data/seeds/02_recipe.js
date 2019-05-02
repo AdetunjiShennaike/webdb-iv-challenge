048afbc41787637226ea1d87df7d11f1a6415350
Adetunji <ashennaike@gmail.com>
2019-05-02 19:22:05 -0400

    fixed filenames and some small typos

--- data/seeds/02_recipe.js
+++ data/seeds/02_recipe.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('recipes').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('recipes').insert([
         { name: 'Miso', instructions: '1. Create the soup base with hon dashi 2. Boil the noodles and after they become soft drain the water 3. Pour in the soup base and boil again to let it soak 4. Add extra toppings such as meats and veggies', notes: '', dish_id: 1 },
         { name: 'Tonkatsu', instructions: '1. Create the soup base with hon dashi 2. Boil the noodles and after they become soft drain the water 3. Pour in the soup base and boil again to let it soak 4. Add extra toppings such as meats and veggies ', notes: '', dish_id: 1 },
         { name: 'Shoyu', instructions: '1. Create the soup base with hon dashi 2. Boil the noodles and after they become soft drain the water 3. Pour in the soup base and boil again to let it soak 4. Add extra toppings such as meats and veggies ', notes: '', dish_id: 1 }
