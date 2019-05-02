afa6783e9c3eb87004079965261858a36f2ca5af
Adetunji <ashennaike@gmail.com>
2019-05-02 15:23:10 -0400

    possibly done wiht db config file, not sure if the process.env is right

---
+++ data/dbConfig.js
@@ -0,0 +1,10 @@
+//import knex and knexfile as well as env
+const knex = require('knex');
+const knexConfig = require('../knexfile');
+require('dotenv').config();
+
+//define the export
+const db = knex(knexConfig[process.env.DB_STAGE])
+
+//export to be used in multiple files instead of having to call repeatedly
+module.exports = db;
